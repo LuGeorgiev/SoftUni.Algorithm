4f158e5d0f48357831169a1dff55ff157494fbac
LuGeorgiev <lubomir_78@abv.bg>
2019-03-02 22:23:56 +0200

    Words 57/100 need to check unique words in smarter way

---
+++ RecursionSortExcercises/SortSearchExcercise/Searching/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Searching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbres = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            var searchFor = 7;
+
+            
+            //int searchedIndex = LineraSearch(numbres, searchFor);
+
+            //Array have to be sorted when using Binary search
+            int searchedIndex = BinarySearch(numbres, searchFor);
+
+            Console.WriteLine(searchedIndex);
+        }
+
+        private static int BinarySearch(int[] numbres, int searchFor)
+        {
+            int index = BinarySearch(numbres, searchFor, 0, numbres.Length - 1);
+            return index;
+        }
+
+        private static int BinarySearch(int[] numbres, int searchFor, int startIndex, int endIndex)
+        {
+            int midIndex = (startIndex + endIndex) / 2;
+            if (numbres[midIndex] == searchFor)
+            {
+                return midIndex;
+            }
+            else if (endIndex - startIndex <= 1 && numbres[endIndex] == searchFor)
+            {
+                return endIndex;
+            }
+            else if (endIndex - startIndex <= 1 && numbres[endIndex] != searchFor)
+            {
+                return -1;
+            }
+
+            if (numbres[midIndex]<searchFor)
+            {
+                return BinarySearch(numbres, searchFor, midIndex, endIndex);
+            }
+            else
+            {
+                return BinarySearch(numbres, searchFor, startIndex, midIndex);
+            }
+        }
+
+        private static int LineraSearch(int[] numbres, int searchFor)
+        {
+            for (int i = 0; i < numbres.Length; i++)
+            {
+                if (numbres[i]==searchFor)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ RecursionSortExcercises/SortSearchExcercise/Words/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Words
+{
+    class Program
+    {
+        static HashSet<string> uniqueWords = new HashSet<string>();
+        static char[] word;
+
+        static void Main(string[] args)
+        {
+            word = Console.ReadLine().ToArray();
+            //int[] vector = Enumerable.Range(0,word.Length).ToArray();
+            GeneratePermutations(word,0);
+
+            Console.WriteLine(uniqueWords.Count);
+        }
+
+        private static void GeneratePermutations(char[] vector,int index)
+        {
+            if (index>=word.Length)
+            {
+                IsValidWord(vector);
+                //Console.WriteLine(string.Join("", vector) );
+            }
+            else
+            {
+                GeneratePermutations(vector, index + 1);
+                for (int i = index+1; i < vector.Length; i++)
+                {
+                    Swap(vector, index, i);
+                    GeneratePermutations(vector, index + 1);
+                    Swap(vector, index, i);
+                }
+            }
+        }
+
+        private static void Swap(char[] vector, int first, int second)
+        {
+            var temp = vector[first];
+            vector[first] = vector[second];
+            vector[second] = temp;
+        }
+
+        private static void IsValidWord(char[] vector)
+        {
+            for (int i = 0; i < vector.Length-1; i++)
+            {
+                if (vector[i]==vector[i+1])
+                {
+                    return;
+                }
+            }
+            uniqueWords.Add(string.Join("",vector));
+        }
+    }
+}
