79e32aa5a71dd88acb8de9b05058e09bc183bef0
LuGeorgiev <lubomir_78@abv.bg>
2019-03-02 22:40:50 +0200

    cannot solve needles

---
+++ RecursionSortExcercises/SortSearchExcercise/Needles/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Needles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lineLength = Console.ReadLine();
+            var needlesLength = Console.ReadLine();
+
+        }
+    }
+}
