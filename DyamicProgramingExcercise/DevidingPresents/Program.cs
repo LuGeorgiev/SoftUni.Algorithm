014c42ea08a3322576334e127dc4851e814d67ab
LuGeorgiev <LuGeorgiev@telenor.bg>
2019-03-18 15:56:46 +0200

    ZigZag and Presects form excercise

---
+++ DyamicProgramingExcercise/DevidingPresents/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace DevidingPresents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var presents = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            var totalSums = presents.Sum();
+            var sums = new int[totalSums + 1];
+            for (int i = 1; i < sums.Length; i++)
+            {
+                sums[i] = -1;
+            }
+
+            for (int presentIndex = 0; presentIndex < presents.Length; presentIndex++)
+            {
+                for (int previousSumIndex = totalSums-presents[presentIndex]; previousSumIndex >= 0; previousSumIndex--)
+                {
+                    if (sums[previousSumIndex]!= -1 
+                        && sums[previousSumIndex+presents[presentIndex]]==-1)
+                    {
+                        //writes dow what is teh index of previous sum that led to this sum
+                        sums[previousSumIndex + presents[presentIndex]] = presentIndex;
+                    }
+                }
+            }
+            var half = totalSums / 2;
+            for (int i = half; i >=0 ; i--)
+            {
+                if (sums[i]==-1)
+                {
+                    continue;
+                }
+                Console.WriteLine($"Difference: {totalSums - i - i}");
+                Console.WriteLine($"Alan:{i} Bob:{totalSums-i}");
+                Console.Write("Alan takes:");
+                while (i!=0)
+                {
+                    Console.Write($"{presents[sums[i]]} ");
+                    i -= presents[sums[i]];
+                }
+                Console.WriteLine();
+                Console.WriteLine("Bob yakes teh rest");
+            }
+
+        }
+    }
+}
---
+++ DyamicProgramingExcercise/LongestZigZagSequence/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LongestZigZagSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var dp = new int[numbers.Length, 2];
+            var prev = new int[numbers.Length, 2];
+
+            dp[0, 0] = dp[0, 1] = 1;
+            prev[0, 0] = prev[0, 1] = -1;
+            var maxResult = 0;
+            var maxindexRow = 0;
+            var maxIndexCol = 0;
+
+            for (int currentIndex = 1; currentIndex < numbers.Length; currentIndex++)
+            {
+                for (int previousIndex = 0; previousIndex < currentIndex; previousIndex++)
+                {
+                    var currentNuumber = numbers[currentIndex];
+                    var previousNumber = numbers[previousIndex];
+
+                    if (currentNuumber > previousNumber && 
+                        dp[currentIndex,0] < dp[previousIndex,1]+1)
+                    {
+                        dp[currentIndex, 0] = dp[previousIndex, 1] + 1;
+                        prev[currentIndex, 0] = previousIndex;
+
+                    }
+
+                    if (currentNuumber<previousNumber && 
+                        dp[currentIndex,1]<dp[previousIndex,0]+1)
+                    {
+                        dp[currentIndex, 1] = dp[previousIndex, 0] + 1;
+                        prev[currentIndex, 1] = previousIndex;
+                    }    
+                }
+
+                if (dp[currentIndex, 0] > maxResult)
+                {
+                    maxResult = dp[currentIndex, 0];
+                    maxindexRow = currentIndex;
+                    maxIndexCol = 0;
+                }
+                if (dp[currentIndex, 1] > maxResult)
+                {
+                    maxResult = dp[currentIndex, 1];
+                    maxindexRow = currentIndex;
+                    maxIndexCol = 1;
+                }
+            }
+
+            var result = new List<int>();
+            while (maxindexRow>=0)
+            {
+                result.Add(numbers[maxindexRow]);
+                maxindexRow = prev[maxindexRow, maxIndexCol];
+                if (maxIndexCol==1)
+                {
+                    maxIndexCol = 0;
+                }
+                else
+                {
+                    maxIndexCol = 1;
+                }
+                result.Reverse();
+            }
+
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
