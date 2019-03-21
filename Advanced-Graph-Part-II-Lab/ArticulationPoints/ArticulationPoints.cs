9ad8b2443f1ff8b549cc35c7a6b96daab7dd7c65
LuGeorgiev <LuGeorgiev@telenor.bg>
2019-03-21 09:59:05 +0200

    articulation point Algo not implemented correctly Tests 3/7

---
+++ Advanced-Graph-Part-II-Lab/ArticulationPoints/ArticulationPoints.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ArticulationPoints
+{
+    //Articulation point is such point that will separete the components if the node is deleted
+    //Hopcroft, Tarjan Algotithm
+
+    private static List<int>[] graph;
+    private static bool[] visited;
+    private static int[] depths;
+    private static int[] lowpoint;
+    private static int?[] parent;
+
+    private static List<int> articulationPoints = new List<int>();
+
+    public static List<int> FindArticulationPoints(List<int>[] targetGraph)
+    {
+        graph = targetGraph;
+        visited = new bool[targetGraph.Length];
+        depths = new int[targetGraph.Length];
+        lowpoint = new int[targetGraph.Length];
+        parent = new int?[targetGraph.Length];
+
+        for (int node = 0; node < graph.Length; node++)
+        {
+            if (!visited[node])
+            {
+                FindArticulationPoints(node, 0);
+            }
+        }
+
+        return articulationPoints;
+    }
+
+    private static void FindArticulationPoints(int node, int depth)
+    {
+        visited[node] = true;
+        depths[node] = depth;
+        lowpoint[node] = depth;
+
+        int childCount = 0;
+        bool isArtriculationPoint = false;
+
+        foreach (var child in graph[node])
+        {
+            if (!visited[node])
+            {
+                parent[child] = node;
+                FindArticulationPoints(child, depth + 1);
+                childCount++;
+
+                if (lowpoint[child] >= depths[node])
+                {
+                    isArtriculationPoint = true;
+                }
+
+                lowpoint[node] = Math.Min(lowpoint[node], lowpoint[child]);
+            }
+            else if (child!=parent[node])
+            {
+                lowpoint[node] = Math.Min(lowpoint[node], depths[child]);
+            }
+        }
+
+        if ((parent[node]==null && childCount>1)
+            || (parent[node]!= null && isArtriculationPoint))
+        {
+            articulationPoints.Add(node);
+        }
+    }
+}
---
+++ Advanced-Graph-Part-II-Lab/StronglyConnectedComponents/StronglyConnectedComponents.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class StronglyConnectedComponents
+{
+    private static List<int>[] graph;
+    private static List<int>[] reveresedGraph;
+    private static bool[] visited;
+    private static Stack<int> stack = new Stack<int>();
+
+    private static List<List<int>> stronglyConnectedComponents;
+
+    public static List<List<int>> FindStronglyConnectedComponents(List<int>[] targetGraph)
+    {
+        graph = targetGraph;
+        visited = new bool[graph.Length];
+        BuildReveresdGraph();
+
+        for (int node = 0; node < graph.Length; node++)
+        {
+            if (!visited[node])
+            {
+                Dfs(node);
+            }
+        }
+
+        stronglyConnectedComponents = new List<List<int>>();
+        visited = new bool[graph.Length];
+        while (stack.Count>0)
+        {
+            var node = stack.Pop();
+            if (!visited[node])
+            {
+                stronglyConnectedComponents.Add(new List<int>());
+                ReversedDfs(node);
+            }
+        }
+        return stronglyConnectedComponents;
+    }
+
+    private static void BuildReveresdGraph()
+    {
+        reveresedGraph = new List<int>[graph.Length];
+        for (int node = 0; node < reveresedGraph.Length; node++)
+        {
+            reveresedGraph[node] = new List<int>();
+        }
+
+        for (int node = 0; node < graph.Length; node++)
+        {
+            foreach (var child in graph[node])
+            {
+                reveresedGraph[child].Add(node);
+            }
+        }
+    }
+
+    private static void ReversedDfs(int node)
+    {
+        if (!visited[node])
+        {
+            visited[node] = true;
+            stronglyConnectedComponents.Last().Add(node);
+
+            foreach (var child in reveresedGraph[node])
+            {
+                ReversedDfs(child);
+            }
+        }
+        
+    }
+
+    private static void Dfs(int node)
+    {
+        if (!visited[node])
+        {
+            visited[node] = true;
+
+            foreach (var child in graph[node])
+            {
+                Dfs(child);
+            }
+            stack.Push(node);
+        }
+    }
+}
