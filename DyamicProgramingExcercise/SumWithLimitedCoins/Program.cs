d9cff503081fef6f97711e4f51d352b0bc6e4f8e
LuGeorgiev <LuGeorgiev@telenor.bg>
2019-03-29 11:59:00 +0200

    Sums with limited amoutnt of coins

---
+++ DyamicProgramingExcercise/SumWithLimitedCoins/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace SumWithLimitedCoins
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] coins = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+            int sum = int.Parse(Console.ReadLine());
+
+            int comb = Findcombinations(coins, sum);
+            Console.WriteLine(comb);
+        }
+
+        private static int Findcombinations(int[] coins, int targetSum)
+        {
+            int[,] maxCount = new int[coins.Length + 1, targetSum + 1];
+            for (int i = 0; i <= coins.Length; i++)
+            {
+                maxCount[i, 0] = 1;
+            }
+
+            for (int coin = 1; coin <= coins.Length; coin++)
+            {
+                for (int sum = targetSum; sum >= 0; sum--)
+                {
+                    if (coins[coin - 1] <= sum && maxCount[coin - 1, sum - coins[coin - 1]] != 0)
+                    {
+                        maxCount[coin, sum]++;
+                    }
+                    else
+                    {
+                        maxCount[coin, sum] = maxCount[coin - 1, sum];
+                    }
+                }
+            }
+          
+
+            int count = 0;
+            for (int i = 0; i <= coins.Length; i++)
+            {
+                if (maxCount[i, targetSum] != 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ DyamicProgramingExcercise/SumWithUnlimitedCoins/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumWithUnlimitedCoins
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] coins = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = int.Parse(Console.ReadLine());
+
+            int combinations = FindCombinationForGivenSum(coins, sum);
+            Console.WriteLine(combinations);
+        }
+
+        private static int FindCombinationForGivenSum(int[] coins, int targetSum)
+        {
+            int[,] maxCombCount = new int[coins.Length + 1, targetSum + 1];
+            for (int i = 0; i <= coins.Length; i++)
+            {
+                maxCombCount[i, 0] = 1;
+            }
+
+            for (int coin = 1; coin <= coins.Length; coin++)
+            {
+                for (int sum = 1; sum <= targetSum; sum++)
+                {
+                    if (coins[coin - 1] <= sum)
+                    {
+                        maxCombCount[coin, sum] = maxCombCount[coin - 1, sum] + maxCombCount[coin, sum - coins[coin - 1]];
+                    }
+                    else
+                    {
+                        maxCombCount[coin, sum] = maxCombCount[coin - 1, sum];
+                    }
+                }
+            }
+            return maxCombCount[coins.Length, targetSum];
+        }
+    }
+}
