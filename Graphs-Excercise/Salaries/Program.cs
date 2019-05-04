d48c368b90c63731466eafe7cf11e6ba06059c11
LuGeorgiev <lubomir_78@abv.bg>
2019-05-04 18:23:18 +0300

    Salaries 50/100 cannot find teh problem

--- Graphs-Excercise/Salaries/Program.cs
+++ Graphs-Excercise/Salaries/Program.cs
@@ -8,13 +8,13 @@ namespace Salaries
     {
         private static Dictionary<int, List<int>> graph;
         private static int[] salaries;
-        private static bool[] visited;
+        //private static bool[] visited;
 
         static void Main(string[] args)
         {
             var numberOfNodes = int.Parse(Console.ReadLine());
             salaries = new int[numberOfNodes];
-            visited = new bool[numberOfNodes];
+            //visited = new bool[numberOfNodes];
             graph = new Dictionary<int, List<int>>();
 
             for (int i = 0; i < numberOfNodes; i++)
@@ -22,52 +22,75 @@ namespace Salaries
                 graph[i] = new List<int>();
             }
 
-            for (int node = 0; node < numberOfNodes; node++)
+            for (int boss = 0; boss < numberOfNodes; boss++)
             {
-                var inputLine = Console.ReadLine().ToCharArray();
+                var inputLine = Console.ReadLine();
                 for (int bossOf = 0; bossOf < numberOfNodes; bossOf++)
                 {
                     if (inputLine[bossOf]=='Y')
                     {
-                        graph[node].Add(bossOf);
+                        graph[boss].Add(bossOf);
                     }
                 }
             }
 
             var employees = graph.Values
                 .SelectMany(x=>x)
+                .Union(graph.Keys)
                 .Distinct()
                 .ToArray();
 
-            var bosses = graph.Keys
-                .Where(x => !employees.Contains(x))
-                .ToArray();
-            foreach (var boss in bosses)
+            foreach (var employee in employees)
             {
-                visited[boss] = true;
-                DFS(boss);
+                DFS(employee);
             }
+
+            //var bosses = graph.Keys
+            //    .Where(x => !employees.Contains(x))
+            //    .ToArray();
+            //foreach (var boss in bosses)
+            //{
+            //    visited[boss] = true;
+            //    DFS(boss);
+            //}
 
             Console.WriteLine(salaries.Sum(x=>x));
         }
 
         private static void DFS(int node)
         {
-            if (graph[node].Count==0)
-            {
-                salaries[node] = 1;
-                return;
+            if (salaries[node]==0)
+            {                
+                if (graph[node].Count>0)
+                {
+                    foreach (var employee in graph[node])
+                    {
+                        DFS(employee);
+                        salaries[node] += salaries[employee];
+                    }
+                }
+                else
+                {
+                    salaries[node] = 1;
+                }
             }
 
-            foreach (var employee in graph[node])
-            {
-                if (!visited[employee])
-                {
-                    visited[employee] = true;
-                    DFS(employee);
-                }
-                salaries[node] += salaries[employee];
-            }
+            
+            //if (graph[node].Count==0)
+            //{
+            //    salaries[node] = 1;
+            //    return;
+            //}
+
+            //foreach (var employee in graph[node])
+            //{
+            //    if (!visited[employee])
+            //    {
+            //        visited[employee] = true;
+            //        DFS(employee);
+            //    }
+            //    salaries[node] += salaries[employee];
+            //}
         }
     }
 }
