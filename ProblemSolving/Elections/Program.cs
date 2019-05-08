ecef6a5a7001cb177cb51c68209f38858add03ae
LuGeorgiev <lubomir_78@abv.bg>
2019-05-08 23:13:04 +0300

    Elections 90/100 maximum

---
+++ ProblemSolving/Elections/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Elections
+{
+    //    You are given the results from the elections.There are N parties that have enough votes and are given seats in the parliament. You are given the seats for each one of the parties.For the parties to have majority in the parliament they need at least K seats (that means K or more seats). Parties can combine with each other in order to have K or more seats together.
+    //Write a program to find the number of all possible combinations of parties with sum of seats K or more.
+
+    //Problem Solve
+    //Find number of combinations (1-N) without repetitions that satisfy цондитион 
+
+    class Program
+    {
+        private static int[] elements;
+        private static int combinationsCount;
+        private static int minSeats;
+
+        static void Main(string[] args)
+        {
+            combinationsCount = 0;
+            minSeats = int.Parse(Console.ReadLine());
+            var parties = int.Parse(Console.ReadLine());
+            elements = new int[parties];
+            for (int i = 0; i < parties; i++)
+            {
+                elements[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 1; i <= elements.Length; i++)
+            {
+                foreach (var combo in Combinations(i, elements.Length))
+                {
+                    CalculatSeatSum(combo);
+                }
+            }
+
+            Console.WriteLine(combinationsCount);
+        }
+
+        private static void CalculatSeatSum(int[] combo)
+        {
+            var sum = 0;
+
+            for (int i = 0; i < combo.Length ; i++)
+            {
+                sum+=elements[combo[i]];
+            }
+
+            if (sum>=minSeats)
+            {
+                combinationsCount++;
+            }
+        }
+
+        private static IEnumerable<int[]> Combinations(int k, int n)
+        {
+            var result = new int[k];
+            var stack = new Stack<int>();
+            stack.Push(0);
+
+            while (stack.Count > 0)
+            {
+                var index = stack.Count - 1;
+                var value = stack.Pop();
+
+                while (value < n)
+                {
+                    result[index++] = value++;
+                    stack.Push(value);
+                    if (index == k)
+                    {
+                        yield return result;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ProblemSolving/ElectionsFasterSolution/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ElectionsFasterSolution
+{
+
+    using System;
+    using System.Linq;
+    using System.Numerics;
+
+    public class Elections
+    {
+        public static void Main()
+        {
+            var targetSum = int.Parse(Console.ReadLine());
+            var numbers = ReadNumbers();
+
+            var sumCounts = CalcSumCounts(numbers);
+
+            var count = CountMatchingSums(targetSum, sumCounts);
+            Console.WriteLine(count);
+        }
+
+        private static BigInteger CountMatchingSums(int targetSum, BigInteger[] sumCounts)
+        {
+            BigInteger count = 0;
+            for (int sum = targetSum; sum < sumCounts.Length; sum++)
+            {
+                count += sumCounts[sum];
+            }
+
+            return count;
+        }
+
+        private static BigInteger[] CalcSumCounts(int[] numbers)
+        {
+            var sumCounts = new BigInteger[numbers.Sum() + 1]; // sum => count possible combinations
+            sumCounts[0] = 1;
+
+            foreach (var number in numbers)
+            {
+                for (int sum = sumCounts.Length - 1; sum >= 0; sum--) // traversing sums backwards
+                {
+                    if (sumCounts[sum] != 0)
+                    {
+                        sumCounts[sum + number] += sumCounts[sum];
+                    }
+                }
+            }
+
+            return sumCounts;
+        }
+
+        private static int[] ReadNumbers()
+        {
+            var numbersCount = int.Parse(Console.ReadLine());
+            var numbers = new int[numbersCount];
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            return numbers;
+        }
+    }
+
+}
