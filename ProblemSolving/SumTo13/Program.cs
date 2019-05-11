cf07acf6ff219ffcde4af75fa601d584fc4abea1
LuGeorgiev <lubomir_78@abv.bg>
2019-05-11 10:06:25 +0300

    SumTo13 too easy

---
+++ ProblemSolving/SumTo13/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace SumTo13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            var numA = numbers[0];
+            var numB = numbers[1];
+            var numC = numbers[2];
+
+            if (numA+numB+numC==13)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if(numA + numB - numC == 13)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (numA - numB + numC == 13)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (numA - numB - numC == 13)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (- numA + numB + numC == 13)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (- numA + numB - numC == 13)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (- numA - numB + numC == 13)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (- numA - numB - numC == 13)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (numA + numB + numC == 13)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            
+        }    
+    }
+}
