ce0c9c1a5de79f55e5169210bebf34b9e0f6ced3
LuGeorgiev <lubomir_78@abv.bg>
2019-05-11 12:55:17 +0300

    Lumber task

---
+++ ProblemSolving/Lumber/LumberMain.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lumber
+{
+    class Program
+    {
+        static List<int>[] graph;
+        static void Main(string[] args)
+        {
+            //Read Input
+            var info = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            var nodes = info[0];
+            var bevers = info[1];
+            graph = new List<int>[nodes];
+            for (int i = 0; i < nodes; i++)
+            {
+                graph[i] = new List<int>();
+            }
+
+            var lumberLogs = new List<Rectangle>();
+            for (int i = 0; i < nodes; i++)
+            {
+                var tokens = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                var currentlumberLog = new Rectangle(i, tokens[0], tokens[1], tokens[2], tokens[3]);
+                lumberLogs.Add(currentlumberLog);
+            }
+
+
+            //BUild Graph
+            foreach (var currentLog in lumberLogs)
+            {
+                foreach (var nextLog in lumberLogs)
+                {
+                    if (currentLog!=nextLog)
+                    {
+                        if (currentLog.AreRectanglesIntersected(nextLog))
+                        {
+                            graph[currentLog.Id].Add(nextLog.Id);
+                        }
+                    }
+                }
+            }
+            var connectedLogs = StronglyConnectedComponnet.FindStronglyConnectedComponents(graph);
+                       
+            
+            for (int i = 0; i < bevers; i++)
+            {
+                var beversPath = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+                int start = beversPath[0] - 1;
+                int end = beversPath[1] - 1;
+
+                var groupOfLogsFofStart = connectedLogs
+                    .FirstOrDefault(x => x.Contains(start));
+                if (groupOfLogsFofStart.Contains(end))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }                    
+            }
+
+        }
+
+    }
+    
+}
---
+++ ProblemSolving/Lumber/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lumber
+{
+    public class Rectangle
+    {
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int id, int ax, int ay, int bx, int by)
+        {
+            this.Id = id;
+            this.Ax = ax;
+            this.Ay = ay;
+            this.Bx = bx;
+            this.By = by;
+        }
+
+        public int Ax { get; private set; }
+        public int Ay { get; private set; }
+        public int Bx { get; private set; }
+        public int By { get; private set; }
+        public int Id { get; private set; }
+
+        public bool AreRectanglesIntersected(Rectangle other)
+        {
+            bool isAIn = this.InPointInRectangle(other.Ax, other.Ay);
+            bool isBIn = this.InPointInRectangle(other.Bx, other.Ay);
+            bool isCIn = this.InPointInRectangle(other.Bx, other.By);
+            bool isDIn = this.InPointInRectangle(other.Ax, other.By);
+
+            return isAIn || isBIn || isCIn || isDIn;
+        }
+
+        private bool InPointInRectangle(int x, int y)
+            => x >= this.Ax
+            && x <= this.Bx
+            && y <= this.Ay
+            && y >= this.By;
+    }
+}
