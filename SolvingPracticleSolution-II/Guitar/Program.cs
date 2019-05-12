1ba6ebdd20a871b91bb9815275ee6cebae7d0919
LuGeorgiev <lubomir_78@abv.bg>
2019-05-12 22:25:24 +0300

    Shortest path with variation iterrative approach

--- SolvingPracticleSolution-II/Guitar/Program.cs
+++ SolvingPracticleSolution-II/Guitar/Program.cs
@@ -19,17 +19,23 @@ namespace Guitar
             matrix = new bool[tones.Length + 1, maxTone + 1];
             matrix[0, initialTone] = true;
 
+            //Collons are all teh possible one values 0-N
+            //Rows are all th etone deviation + 1 (one more for teh initial state)
             for (int i = 0; i < tones.Length; i++)
             {
                 var currentTone = tones[i];
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
+                    //Finds the possibilities from previous tone add/substract
                     if (matrix[i,j])
                     {
+                        //If it is possible lowers tone
                         if (j-currentTone>=0)
                         {
                             matrix[i + 1, j - currentTone] = true;
                         }
+
+                        //If it is possibles increasese tone
                         if (j+currentTone<=maxTone)
                         {
                             matrix[i + 1, j + currentTone] = true;
@@ -39,6 +45,7 @@ namespace Guitar
             }
             bool isToneFount = false;
             int lastRol = matrix.GetLength(0) - 1;
+            //finds the highest possible tone from all possible deviations
             for (int i = matrix.GetLength(1)-1; i >= 0; i--)
             {
                 if (matrix[lastRol, i])
---
+++ SolvingPracticleSolution-II/ShortestPath/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShortestPath
+{
+    class Program
+    {
+        private static char[] directions = new[] {'L','R','S'};
+        private static char[] input;
+        private static Queue<string> queue;
+        static void Main(string[] args)
+        {
+            input = Console.ReadLine().ToCharArray();
+            var asterixCount = input.Where(x => x == '*').Count();
+            queue = new Queue<string>();
+
+            GenerateVariationsWithRepetition(asterixCount);
+
+            Console.WriteLine(queue.Count);
+            while (queue.Count>0)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+
+        private static void Print(int[] elements)
+        {
+            //Console.WriteLine(string.Join(", ",elements));
+            var counter = 0;
+            var result = new char[input.Length];
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (input[i] != '*')
+                {
+                    result[i] = input[i];
+                }
+                else
+                {
+                    result[i] = directions[elements[counter]];
+                    counter++;
+                }
+            }
+            queue.Enqueue(new string(result));
+        }
+
+        private static void GenerateVariationsWithRepetition(int symbolsToFill)
+        {
+            int n = 3;
+            int k = symbolsToFill; // L R S
+            int[] arr = new int[k];
+
+            while (true)
+            {
+                int index = k - 1;
+                Print(arr);
+                while (index >= 0 && arr[index] != n - 1)
+                {
+                    arr[index]++;
+                    Print(arr);
+                }
+                for (int i = index; i >= 0; i--)
+                {
+                    if (arr[i] >= n - 1)
+                    {
+                        arr[i] = 0;
+                        arr[i - 1]++;
+                    }
+                    if (arr[i - 1] <= n - 1 || arr[0] >= n)
+                    {
+                        break;
+                    }
+
+                }
+
+                if (arr[0] >= n)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
