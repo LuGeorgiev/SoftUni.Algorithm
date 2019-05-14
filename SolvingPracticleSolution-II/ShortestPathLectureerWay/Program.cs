752f0f9d05ab5a13ee1bd4b2d3de5f1ad82af0f8
LuGeorgiev <lubomir_78@abv.bg>
2019-05-14 22:21:50 +0300

    Sortest path second way. Much faster

---
+++ SolvingPracticleSolution-II/ShortestPathLectureerWay/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShortestPathLectureerWay
+{
+    class Program
+    {
+        private static List<int> indeces = new List<int>();
+        private static char[] directions = new[] {'L','R','S' };
+        private static List<string> maps = new List<string>();
+
+        static void Main(string[] args)
+        {
+            var map = Console.ReadLine().ToCharArray();
+            for (int i = 0; i < map.Length; i++)
+            {
+                if (map[i]=='*')
+                {
+                    indeces.Add(i);
+                }
+            }
+
+            FindMap(map, 0);
+            Console.WriteLine(maps.Count);
+            Console.WriteLine(string.Join(Environment.NewLine, maps));
+        }
+
+        private static void FindMap(char[] map, int currentIndex)
+        {
+            if (currentIndex==indeces.Count)
+            {
+                maps.Add(string.Join("", map));
+                return;
+            }
+
+            foreach (var direction in directions)
+            {
+                map[indeces[currentIndex]] = direction;
+                FindMap(map, currentIndex + 1);
+            }
+        }
+    }
+}
