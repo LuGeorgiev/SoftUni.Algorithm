c639fa8af20dfa66dd982c926a69fa60d624bf27
LuGeorgiev <lubomir_78@abv.bg>
2019-05-21 23:22:57 +0300

    Shooting range two solutions

---
+++ ExamPrep20082017/ExamPreparation/ShootingRange/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShootingRange
+{
+    class Program
+    {
+        private static int[] elements;
+
+        private static int targetSum;
+
+        private static HashSet<string> finalPermutation = new HashSet<string>();
+
+        static void Main(string[] args)
+        {
+            ReadElements();
+            targetSum = int.Parse(Console.ReadLine());
+            Permute(0);
+
+            foreach (var permutation in finalPermutation)
+            {
+                Console.WriteLine(permutation);
+            }
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                PrintIfTargetReached();
+            }
+            else
+            {
+                Permute(index + 1);
+
+                for (int current = index + 1; current < elements.Length; current++)
+                {
+                    Swap(index, current);
+                    Permute(index + 1);
+                    Swap(index, current);
+                }
+            }
+        }
+
+        private static void Swap(int first, int second)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+
+        private static void ReadElements()
+            => elements = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        private static void PrintIfTargetReached()
+        {
+
+            int curentSum = 0;
+            for (int i = 1; i <= elements.Length; i++)
+            {
+                curentSum += elements[i - 1] * i;
+                if (curentSum == targetSum)
+                {
+                    var result = string.Join(" ", elements).Substring(0, 2 * i - 1);
+                    finalPermutation.Add(result);
+
+                    return;
+                }
+                else if (curentSum > targetSum)
+                {
+                    return;
+                }
+            }
+        }
+
+    }
+}
---
+++ ExamPrep20082017/ExamPreparation/ShootingRangeFastSolution/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShootingRangeFastSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            bool[] marked = new bool[values.Length];
+
+            int target = int.Parse(Console.ReadLine());
+
+            GenerateSequences(0, target, values, marked);
+        }
+
+        private static void GenerateSequences(int index, int target, int[] values, bool[] marked)
+        {
+            int score = GetScore(values, marked);
+
+            if (score == target)
+            {
+                Print(values, marked);
+            }
+            if (index >= values.Length || score >= target)
+            {
+                return;
+            }
+
+            HashSet<int> swapped = new HashSet<int>();
+            for (int i = index; i < values.Length; i++)
+            {
+                if (!swapped.Contains(values[i]))
+                {
+                    Swap(index, i, values);
+                    marked[index] = true;
+
+                    GenerateSequences(index + 1, target, values, marked);
+
+                    Swap(index, i, values);
+                    marked[index] = false;
+
+                    swapped.Add(values[i]);
+                }
+            }
+
+        }
+
+        private static void Swap(int i, int j, int[] values)
+        {
+            var temp = values[i];
+            values[i] = values[j];
+            values[j] = temp;
+        }
+
+        private static void Print(int[] values, bool[] marked)
+        {
+            var result = "";
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (marked[i])
+                {
+                    result+=values[i] + " ";
+                }
+            }
+            Console.WriteLine(result.Trim());
+        }
+
+        private static int GetScore(int[] values, bool[] marked)
+        {
+            int score = 0;
+            int multiplyer = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (marked[i] == true)
+                {
+                    score += values[i] * ++multiplyer;
+                }
+            }
+
+            return score;
+        }
+    }
+}
