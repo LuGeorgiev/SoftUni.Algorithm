4ffae08ecb0b0994274e72c1e5dce51399e439aa
LuGeorgiev <lubomir_78@abv.bg>
2019-05-29 21:56:03 +0300

    mine slow solution 20/100 and authors

---
+++ ExamPrep13052018/Exam13052018/ZergAuthorSolution/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Numerics;
+
+namespace ZergAuthorSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lineParts = Console.ReadLine().Split(' ');
+            int N = int.Parse(lineParts[0]);
+            int M = int.Parse(lineParts[1]);
+            lineParts = Console.ReadLine().Split(' ');
+            int Fx = int.Parse(lineParts[0]);
+            int Fy = int.Parse(lineParts[1]);
+            int K = int.Parse(Console.ReadLine());
+
+            BigInteger[,] board = new BigInteger[N, M];
+            for (int i = 0; i < K; i++)
+            {
+                lineParts = Console.ReadLine().Split(' ');
+                int x = int.Parse(lineParts[0]);
+                int y = int.Parse(lineParts[1]);
+                board[x, y] = -1;
+            }
+
+            bool doge = false;
+            for (int i = 0; i < N; i++)
+            {
+                if (board[i, 0] != -1 && !doge)
+                {
+                    board[i, 0] = 1;
+                }
+                else
+                {
+                    doge = true;
+                    board[i, 0] = 0;
+                }
+            }
+
+            doge = false;
+            for (int i = 0; i < M; i++)
+            {
+                if (board[0, i] != -1 && !doge)
+                {
+                    board[0, i] = 1;
+                }
+                else
+                {
+                    doge = true;
+                    board[0, i] = 0;
+                }
+            }
+
+            for (int i = 1; i < N; i++)
+            {
+                for (int j = 1; j < M; j++)
+                {
+                    if (board[i, j] != -1)
+                    {
+                        board[i, j] = board[i - 1, j] + board[i, j - 1];
+                    }
+                    else
+                    {
+                        board[i, j] = 0;
+                    }
+                }
+            }
+
+            Console.WriteLine(board[Fx, Fy]);
+        }
+    }
+}
--- ExamPrep13052018/Exam13052018/ZergZerg/Program.cs
+++ ExamPrep13052018/Exam13052018/ZergZerg/Program.cs
@@ -5,7 +5,7 @@ namespace ZergZerg
 {
     class Program
     {
-        private static char[,] matrix;
+        private static int[,] matrix;
         private static int rows;
         private static int cols;
         private static int counter = 0;
@@ -18,15 +18,16 @@ namespace ZergZerg
                 .ToArray();
             rows = input[0];
             cols = input[1];
-            matrix = new char[rows, cols];
+            matrix = new int[rows, cols];
 
             var baseCoordinates = Console.ReadLine()
                 .Split()
                 .Select(int.Parse)
                 .ToArray();
-            matrix[baseCoordinates[0], baseCoordinates[1]] = 'B';
-            matrix[0, 0] = 'V';
-
+            var baseRow = baseCoordinates[0];
+            var baseCol = baseCoordinates[1];
+            matrix[baseRow, baseCol] = -2;
+            
             var numberOfEnemies = int.Parse(Console.ReadLine());
             for (int i = 0; i < numberOfEnemies; i++)
             {
@@ -34,31 +35,175 @@ namespace ZergZerg
                 .Split()
                 .Select(int.Parse)
                 .ToArray();
-
-                matrix[input[0], input[1]] = 'E';
+                // Enemy
+                matrix[input[0], input[1]] = -1;
             }
 
+            // 20/100 in judge
             DFS(0, 0);
             Console.WriteLine(counter);
+
+            // NOT WORKING
+            //Dynamic(baseRow, baseCol);
+            //if (matrix[baseRow-1,baseCol]>0 && matrix[baseRow, baseCol-1] > 0)
+            //{
+            //    counter = matrix[baseRow - 1, baseCol] + matrix[baseRow, baseCol - 1];
+            //}
+            //else if(matrix[baseRow - 1, baseCol] == -1 && matrix[baseRow, baseCol - 1] == -1)
+            //{
+            //    counter = 0;
+            //}
+            //else
+            //{
+            //    counter =Math.Max( matrix[baseRow - 1, baseCol] , matrix[baseRow, baseCol - 1]);
+            //}
+            //Console.WriteLine(counter);
+        }
+
+        private static void Dynamic(int baseRow, int baseCol)
+        {
+            if (baseRow == 0 && baseCol == 1 || baseRow == 1 && baseCol == 0)
+            {
+                counter = 1;
+                return;
+            }
+
+            if (matrix[0,1] != -1)
+            {
+                matrix[0, 1] = 1;
+            }
+            if (matrix[1, 0] != -1)
+            {
+                matrix[1, 0] = 1;
+            }
+
+            //fill first row
+            for (int row = 2; row <= baseRow; row++)
+            {               
+                if (matrix[row-1,0]== -1 || matrix[row, 0] == -1)
+                {
+                    matrix[row, 0] = -1;
+                    continue;
+                }
+
+                bool canPlaceDown = false;
+                if (row+1<=baseRow && matrix[row+1,0]!=-1)
+                {
+                    canPlaceDown = true;
+                }
+                bool canPlaceRight = false;
+                if (matrix[row,1]!=-1)
+                {
+                    canPlaceRight = true;
+                }
+
+                if (canPlaceRight && canPlaceDown)
+                {
+                    matrix[row, 0] = matrix[row - 1, 0] + 1;
+                }
+                else if (canPlaceRight ^ canPlaceDown)
+                {
+                    matrix[row, 0] = matrix[row - 1, 0];
+                }
+                else
+                {
+                    matrix[row, 0] = -1;
+                }
+            }
+
+            //fill first coll
+            for (int col = 2; col <= baseCol; col++)
+            {
+                if (matrix[0, col - 1] == -1 || matrix[0,col]==-1)
+                {
+                    matrix[col, 0] = -1;
+                    continue;
+                }
+
+                bool canPlaceDown = false;
+                if (col + 1 <= baseCol && matrix[0, col+1] != -1)
+                {
+                    canPlaceDown = true;
+                }
+                bool canPlaceRight = false;
+                if (matrix[1, col] != -1)
+                {
+                    canPlaceRight = true;
+                }
+
+                if (canPlaceRight && canPlaceDown)
+                {
+                    matrix[0, col] = matrix[0, col-1] + 1;
+                }
+                else if (canPlaceRight ^ canPlaceDown)
+                {
+                    matrix[0, col] = matrix[0, col-1];
+                }
+                else
+                {
+                    matrix[0, col] = -1;
+                }
+            }
+
+            for (int row = 1; row <= baseRow; row++)
+            {
+                for (int col = 1; col <= baseCol; col++)
+                {
+                    if (matrix[row,col]==-1)
+                    {
+                        continue;
+                    }
+                    var curentValue = Math.Max(matrix[row - 1, col], matrix[row, col - 1]);
+                    if (curentValue==-1)
+                    {
+                        matrix[row, col] = -1;
+                        continue;
+                    }
+
+                    bool canPlaceDown = false;
+                    if (row + 1 <= baseRow && matrix[row + 1, 0] != -1)
+                    {
+                        canPlaceDown = true;
+                    }
+                    bool canPlaceRight = false;
+                    if (matrix[row, 1] != -1)
+                    {
+                        canPlaceRight = true;
+                    }
+
+                    if (canPlaceRight && canPlaceDown)
+                    {
+                        matrix[row, col] = curentValue + 1;
+                    }
+                    else if (canPlaceRight ^ canPlaceDown)
+                    {
+                        matrix[row, col] = curentValue;
+                    }
+                    else
+                    {
+                        matrix[row, col] = -1;
+                    }
+                }
+            }
         }
 
         private static void DFS(int row, int col)
         {
 
-            if (matrix[row,col]=='B')
+            if (matrix[row,col]==-2)
             {
                 counter++;
                 return;
             }
 
             // Move Right
-            if (col + 1 < cols && matrix[row, col + 1] != 'E' )
+            if (col + 1 < cols && matrix[row, col + 1] != -1 )
             {
                 DFS(row, col + 1);
             }
 
             // Move down
-            if (row + 1 < rows && matrix[row+1, col] != 'E' )
+            if (row + 1 < rows && matrix[row+1, col] != -1 )
             {
                 
                 DFS(row + 1, col);
