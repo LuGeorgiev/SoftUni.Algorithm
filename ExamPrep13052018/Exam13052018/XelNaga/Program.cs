052cf60bb211554d25d258eedeb7897a4421fb6a
LuGeorgiev <lubomir_78@abv.bg>
2019-05-30 22:54:29 +0300

    46/100 in judge the solution is not correct

---
+++ ExamPrep13052018/Exam13052018/XelNaga/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace XelNaga
+{
+    class Program
+    {
+        private static Dictionary<int, int> spicesCount = new Dictionary<int, int>();
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Where(x => x != "-1")
+                .Select(int.Parse)
+                .ToArray();
+            ;
+            spicesCount.Add(input[0], 1);
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i]==input[i-1])
+                {
+                    continue;
+                }
+
+                if (!spicesCount.ContainsKey(input[i]))
+                {
+                    spicesCount[input[i]] = 1;
+                    continue;
+                }
+
+                spicesCount[input[i]]++;
+            }
+
+            long result = 0;
+            foreach (var kvp in spicesCount)
+            {
+                result += (kvp.Key + 1) * kvp.Value;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
