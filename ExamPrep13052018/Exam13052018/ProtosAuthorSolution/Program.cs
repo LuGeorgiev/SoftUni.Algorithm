6d2a9206ae5309b970fc437f74c6cc3c1f33821b
LuGeorgiev <lubomir_78@abv.bg>
2019-05-30 06:15:27 +0300

    author's soluthion

---
+++ ExamPrep13052018/Exam13052018/ProtosAuthorSolution/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ProtosAuthorSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = int.Parse(Console.ReadLine());
+            var adjMatrix = new bool[c, c];
+
+            // Read matrix
+            for (int i = 0; i < c; i++)
+            {
+                string line = Console.ReadLine();
+                for (int j = 0; j < c; j++)
+                {
+                    adjMatrix[i, j] = line[j] == 'Y';
+                }
+            }
+
+            // Solve task
+            int maxNumberOfTwoFriends = 0;
+            for (int row = 0; row < c; row++)
+            {
+                int numberOfTwoFriends = 0;
+                for (int col = 0; col < c; col++)
+                {
+                    bool areTwoFriends = false;
+
+                    if (row == col)
+                    {
+                        continue;
+                    }
+
+                    if (adjMatrix[row, col])
+                    {
+                        areTwoFriends = true;
+                    }
+                    else
+                    {
+                        for (int k = 0; k < c; k++)
+                        {
+                            if ((k != row) && (k != col))
+                            {
+                                if (adjMatrix[row, k] && adjMatrix[col, k])
+                                {
+                                    areTwoFriends = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    if (areTwoFriends)
+                    {
+                        numberOfTwoFriends++;
+                    }
+                }
+
+                maxNumberOfTwoFriends = Math.Max(maxNumberOfTwoFriends, numberOfTwoFriends);
+            }
+
+            // Output solution
+            Console.WriteLine(maxNumberOfTwoFriends);
+        }
+    }
+}
